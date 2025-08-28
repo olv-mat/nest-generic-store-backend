cd6ad24c45e464f795ee209123528ac56cf22d72
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-08-28 14:32:32 -0300

    feat: endpoint to find all users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  public async findAll(): Promise<UserEntity[]> {
+    return await this.userService.findAll();
+  }
 }
