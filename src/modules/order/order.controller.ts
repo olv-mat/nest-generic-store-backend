0901e50b91ed6dd326ea4fca359abc2fc7d0b508
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-09-02 16:04:15 -0300

    feat: endpoint to find all orders

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { OrderEntity } from './entities/order.entity';
 
 @Controller('order')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  public async findAll(): Promise<OrderEntity[]> {
+    return this.orderService.findAll();
+  }
 }
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrderEntity } from './entities/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(
+    @InjectRepository(OrderEntity)
+    private readonly orderRepository: Repository<OrderEntity>,
+  ) {}
+
+  public async findAll(): Promise<OrderEntity[]> {
+    return this.orderRepository.find();
+  }
+}
