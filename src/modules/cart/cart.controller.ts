2ad55da7b74602e32abc87dba85894acf9909519
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-09-16 08:45:17 -0300

    feat: endpoint to find all carts

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CartService } from './cart.service';
+import { CartEntity } from './entities/cart.entity';
 
-@Controller('cart')
+@Controller('carts')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Get()
+  public async findAll(): Promise<CartEntity[]> {
+    return this.cartService.findAll();
+  }
 }
--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CartEntity } from './entities/cart.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(
+    @InjectRepository(CartEntity)
+    private readonly cartRepository: Repository<CartEntity>,
+  ) {}
+
+  public async findAll(): Promise<CartEntity[]> {
+    return this.cartRepository.find();
+  }
+}
